81638f28762446011312bf89b297003b94df6b6e
Jryke <rykerson@gmail.com>
2019-08-29 20:17:36 +0700

    shot count added, point code moved to points.js

---
+++ points.js
@@ -0,0 +1,24 @@
+const bullseye = document.getElementsByClassName('bullseye')
+const secondCircle = document.getElementsByClassName('second-circle')
+const thirdCircle = document.getElementsByClassName('third-circle')
+const fourthCircle = document.getElementsByClassName('fourth-circle')
+const fifthCircle = document.getElementsByClassName('fifth-circle')
+const sixthCircle = document.getElementsByClassName('sixth-circle')
+const seventhCircle = document.getElementsByClassName('seventh-circle')
+const eigthCircle = document.getElementsByClassName('eigth-circle')
+const ninthCircle = document.getElementsByClassName('ninth-circle')
+const tenthCircle = document.getElementsByClassName('tenth-circle')
+
+let circlesArr = [bullseye, secondCircle, thirdCircle, fourthCircle, fifthCircle, sixthCircle, seventhCircle, eigthCircle, ninthCircle, tenthCircle]
+
+let points = 0
+
+// click events for circles
+circlesArr.forEach(circle => {
+	Array.from(circle).forEach(function(element) {
+	  element.addEventListener('click', () => {
+			points += 1
+			document.getElementById('score').textContent = points.toString()
+		});
+	});
+})
--- shoot.js
+++ shoot.js
@@ -1,32 +1,7 @@
-const bullseye = document.getElementsByClassName('bullseye')
-const secondCircle = document.getElementsByClassName('second-circle')
-const thirdCircle = document.getElementsByClassName('third-circle')
-const fourthCircle = document.getElementsByClassName('fourth-circle')
-const fifthCircle = document.getElementsByClassName('fifth-circle')
-const sixthCircle = document.getElementsByClassName('sixth-circle')
-const seventhCircle = document.getElementsByClassName('seventh-circle')
-const eigthCircle = document.getElementsByClassName('eigth-circle')
-const ninthCircle = document.getElementsByClassName('ninth-circle')
-const tenthCircle = document.getElementsByClassName('tenth-circle')
+let shots = 0
 
-let circlesArr = [bullseye, secondCircle, thirdCircle, fourthCircle, fifthCircle, sixthCircle, seventhCircle, eigthCircle, ninthCircle, tenthCircle]
-
-let points = 0
-
-// click events for circles
-circlesArr.forEach(circle => {
-	Array.from(circle).forEach(function(element) {
-	  element.addEventListener('click', () => {
-			points += 1
-			document.getElementById('score').textContent = points.toString()
-		});
-	});
+// click event for shots
+document.getElementById('background').addEventListener('click', () => {
+	shots ++
+	document.getElementById('shot-count').textContent = shots.toString()
 })
-
-// let circlesArr = [{bullseye: bullseye}, {secondCircle: secondCircle}, {thirdCircle: thirdCircle}, {fourthCircle: fourthCircle}, {fifthCircle: fifthCircle}, {sixthCircle: sixthCircle}, {seventhCircle: seventhCircle}, {eigthCircle: eigthCircle}, {ninthCircle: ninthCircle}, {tenthCircle: tenthCircle}]
-
-// Array.from(bullseye).forEach(function(element) {
-//   element.addEventListener('click', () => {
-// 		console.log('bullseye clicked!')
-// 	});
-// });
